8c9afddad897cc11eb40d9038689d20a1e1262ec
pete hotchkiss <pete@pheekaboo.io>
2016-07-20 11:31:27 +0100

    nuked a bunch of console.logs cluttering the main app JS file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,6 @@ angular.module('cashPointApp', [])
     });
     $scope.locale = 'en-gb';
     $scope.withdrawlpriortiy = '##buildtype##';
-    // $scope.withdrawlpriortiy = ##buildtype##;
     $scope.prioritydenomination = Number('##priority-value##');
     $scope.currentbalance = 0;
     $scope.transactions = []; // to hold transactional history
@@ -68,14 +67,12 @@ angular.module('cashPointApp', [])
     };
 
     /***
-    Returns boolean value to determine in requested withdrawl amount is available
+    Returns boolean value to determine in requested withdrawl amount is available. No free overdrafts at this bank
     @method checkAvailable
     @param a { number } - withdrawl amount requested
     */
     $scope.checkAvailableBalance = function( a ) {
-      // console.log("requested amoutn", a, $scope.balance());
       return $scope.currentbalance > a;
-      // return true;
     };
 
     /**
@@ -94,25 +91,26 @@ angular.module('cashPointApp', [])
 
     /***
     Withdraw a passed amount. Returns a results object with details of how the cash withdrawl is structured, and the remaining balance in the float.
+
     @method withdraw
     @param a { number } amount to withdraw
     */
     $scope.withdraw = function( a ) {
       if( isNaN(a) ) {
-        // now a number so throw an appropriate error
+        // This should never happen, but better to be safe
         throw ( new Error('Sorry - only numbers can be withdrawn'));
       }
 
       if( !$scope.checkAvailableBalance( a ) ) {
+        // Requested amount is more than the avilable balance.
         throw ( new Error('Sorry - Insuficent funds') );
       } else if ( a < 1 ) {
+        // Cant dispense fresh air so ensure that at least something have been requested
         throw ( new Error('Sorry - you need to request an ammount of at least £0.01'));
       } else {
         // there's enough money avilable so get a withdrawl
         var wd = $scope.getRequestedWithdrawl(a);
         var cn = $scope.getWithdrawlCountsTypeCounts( a );
-
-        // console.log("wd", wd);
 
         // update the float so it relfects the withdrawl
         $scope.updateFloat( wd );
@@ -121,9 +119,12 @@ angular.module('cashPointApp', [])
         var t = { timestamp: Date.now(), amount: $scope.formatAsCurrency(a), balance: $scope.formatAsCurrency($scope.currentbalance), totalcount: wd.length, withdrawldetail: $scope.getRequestedWithdrawlCounts( wd ),
         breakdown: cn, simple: wd }; // singleOrDefault
 
-
+        // dump the latest transaction into the transaction history
         $scope.transactions.unshift(t);
+        // reset everything
         $scope.reset();
+
+        // push a message to the UI
         $scope.message = { type: 'sucsess', message: 'Sucsessful Transaction' };
         return t;
       }
@@ -133,33 +134,31 @@ angular.module('cashPointApp', [])
     /***
     Returns a simple array detailing the structure of the returned withdrawl. Values are returned as their raw values with the largest denomination first i.e. [100,50,20,20,10,5,2,1]
 
+    @param getRequestedWithdrawl
+    @param a { numer } - the amount to withdraw
     @param ty = withdrawl return type. Either "denomination" or "type" - i.e. do you want a list of values of types
     */
     $scope.getRequestedWithdrawl = function( a, ty ) {
-      // console.log('getRequestedWithdrawl()', a);
+
       // grab an array of the currently available denominations
       var bills = $scope.getAviailableDenominations(true);
 
-      // the withdrawl algorithm impacts whether a given denomination whould be prioritised or just the fewest possible notes/coins
+      // the withdrawl algorithm impacts whether a given denomination whould be prioritised or just the fewest possible notes/coins. If we're running in least mode then use the entire float available. If we're in denomination priority mode then only allow the loop to count back from the index where the desired denomination exists
       var index = ($scope.withdrawlpriortiy === 'least') ? bills.length - 1 : $scope.getPriorityIndex($scope.prioritydenomination);
 
       var splits = [];
 
       var cp = 1;
-      // console.log('float', $scope.float, $scope.withdrawlpriortiy);
-      // console.log('bills', bills[0]);
-      // loop through available denominations to try and make total
+
+      // start building the transaction
       while (a >= bills[0].denomination){
 
-        // console.log("Amount", bills[index].denomination, $scope.float[index].amount, cp, $scope.float[index].amount >= cp);
-
-
-        // console.log("a", a,bills[index].denomination)
+        // keep adding instances of the current denomination untill such a point it's more than required so step down to next smallest denomination
         if (a >= bills[index].denomination && $scope.float[index].amount >= cp){
-          //
+
+          // build value object to be returned to the callig method dependent on what the callee requires. Sometimes it's just the denomination value - and othertimes it's more verbose and the type of denomination the value is ( i.e. coin or note ) is also required.
           a -= bills[index].denomination;
           splits.push( (ty !== 'type') ? bills[index].denomination : { type: bills[index].type, value: bills[index].denomination } );
-          // splits.push( (ty !== 'type') ? bills[index].denomination : bills[index].type );
 
           cp++;
         } else {
@@ -169,12 +168,10 @@ angular.module('cashPointApp', [])
             if( bills[index].denomination === 1 && a !== 0 ) {
               throw ( new Error('Sorry - we cant provide that withdrawl amount. The float is ' + $scope.formatAsCurrency( a ) + ' short' ) );
             } else {
-              // console.log('there', a);
               index--;
             }
         }
       }
-      // console.log("a", a);
       return splits;
     };
 
@@ -185,7 +182,6 @@ angular.module('cashPointApp', [])
 
     */
     $scope.getRequestedWithdrawlCounts = function( a ) {
-      // console.log("arg", a);
       return jslinq( a )
       .groupBy( function(e) {
         return e;
@@ -262,6 +258,7 @@ angular.module('cashPointApp', [])
     $scope.getPriorityIndex = function( d ) {
       // loops through the float, finding the position of the denomination that should be given priority
       var ind = $scope.getAviailableDenominations(false).indexOf(d);
+
       // as long as there is stock of the priority denomination keep using it
       return ( $scope.float[ind].amount > 1 ) ? ind : $scope.float.length - 1;
     };
