319099df08fb3358221d996f05af12b400a523c8
pete hotchkiss <pete@pheekaboo.io>
2016-07-21 21:13:32 +0100

    nuked a bunch of eslint errors in the Jasmine tests

--- tests/02.cashpoint.controller.public-methods.js
+++ tests/02.cashpoint.controller.public-methods.js
@@ -1,8 +1,11 @@
+/* global inject */
+'use strict';
+
 describe('CaashPoint', function () {
 
   beforeEach(module('cashPointApp'));
 
-  var $controller, httpBackend;
+  var $controller;
 
 
   beforeEach(inject(function(_$controller_){
@@ -11,21 +14,23 @@ describe('CaashPoint', function () {
 
   describe('Controller public methods:', function () {
 
-    var $scope, controller
+    var $scope;
 
     beforeEach(inject(function(){
+      var controller;
+
       $scope = {};
       controller = $controller('cashPointController', { $scope: $scope });
 
       $scope.withdrawlpriortiy = 'least';
       $scope.prioritydenomination = 2000;
-      $scope.locale = "en-gb";
+      $scope.locale = 'en-gb';
     }));
 
     it('withdraw() should throw an exception if anthing other than a number is passed', function() {
 
-      expect(function() { $scope.withdraw('foo') }).toThrow(new Error('Sorry - only numbers can be withdrawn'));
-    })
+      expect( function() { $scope.withdraw('foo'); }).toThrow( new Error('Sorry - only numbers can be withdrawn'));
+    });
 
     //
     it('withdraw() should throw an exception if there is insufficent funds to cover withdrawl', function () {
@@ -34,40 +39,40 @@ describe('CaashPoint', function () {
 
       // try and withdraw 1000 from a balance of 468
       $scope.currentbalance = 500;
-      expect(function() { $scope.withdraw(1005) }).toThrow(new Error('Sorry - Insuficent funds'));
+      expect(function() { $scope.withdraw(1005); }).toThrow(new Error('Sorry - Insuficent funds'));
 
       // try and withdraw 57 from a balance of 7934
       $scope.currentbalance = 4000;
-      expect(function() { $scope.withdraw(1000) }).not.toThrow(new Error('Sorry - Insuficent funds'));
-    })
+      expect(function() { $scope.withdraw(1000); }).not.toThrow(new Error('Sorry - Insuficent funds'));
+    });
 
     it('withdraw() throws error if withdrawl amount cant be made up from available float', function() {
 
       $scope.currentbalance = 465;
-      $scope.float = [ {"denomination": 1, "amount": 5}, {"denomination": 20, "amount": 3} , { "denomination": 100, "amount": 4}];
+      $scope.float = [ {'denomination': 1, 'amount': 5}, {'denomination': 20, 'amount': 3}, { 'denomination': 100, 'amount': 4} ];
 
-      expect(function() { $scope.withdraw(446) }).toThrow(new Error('Sorry - we cant provide that withdrawl amount. The float is £0.01 short'));
+      expect(function() { $scope.withdraw(446); }).toThrow(new Error('Sorry - we cant provide that withdrawl amount. The float is £0.01 short'));
 
-    })
+    });
     //
     //
     it('withdraw() can withdraw be made up', function() {
       $scope.currentbalance = 465;
-      $scope.float = [ {"denomination": 1, "amount": 5}, {"denomination": 20, "amount": 3} , { "denomination": 100, "amount": 4}];
+      $scope.float = [ {'denomination': 1, 'amount': 5}, {'denomination': 20, 'amount': 3}, { 'denomination': 100, 'amount': 4}];
 
-      expect($scope.withdraw(64).balance).toBe("£4.01");
+      expect($scope.withdraw(64).balance).toBe('£4.01');
 
-    })
+    });
 
     // //
     it('withdraw() returns an expected float balance', function() {
 
       // withdrawing 68 from a balance of 468 returns the epxected amount
       $scope.currentbalance = 468;
-      $scope.float = [ {"denomination": 1, "amount": 8, "type": "coin"}, {"denomination": 20, "amount": 3, "type": "coin"} , { "denomination": 100, "amount": 4, "type": "coin"}];
+      $scope.float = [ {'denomination': 1, 'amount': 8, 'type': 'coin'}, {'denomination': 20, 'amount': 3, 'type': 'coin'}, { 'denomination': 100, 'amount': 4, 'type': 'coin'}];
 
 
-      expect($scope.withdraw(68).balance).toBe("£4.00");
+      expect($scope.withdraw(68).balance).toBe('£4.00');
     });
     // //
     it('withdraw() returns an object with expected structure', function() {
@@ -76,7 +81,7 @@ describe('CaashPoint', function () {
 
       // withdrawing 68 from a balance of 468 returns the epxected amount
       $scope.currentbalance = 4468;
-      $scope.float = [ {"denomination": 1, "amount": 8, "type": "coin"}, {"denomination": 20, "amount": 3, "type": "coin"}, { "denomination": 100, "amount": 4, "type": "coin"}, { "denomination": 1000, "amount": 4, "type": "note"}];
+      $scope.float = [ {'denomination': 1, 'amount': 8, 'type': 'coin'}, {'denomination': 20, 'amount': 3, 'type': 'coin'}, { 'denomination': 100, 'amount': 4, 'type': 'coin'}, { 'denomination': 1000, 'amount': 4, 'type': 'note'}];
       var nw = $scope.withdraw(1268);
       expect(nw).toHaveMember('balance');
       expect(nw).toHaveMember('totalcount');
@@ -84,21 +89,21 @@ describe('CaashPoint', function () {
       expect(nw).toHaveMember('breakdown');
       // expect(nw.breakdown).toEqual({coins: 13, notes: 1});
       // console.log("detail: ", nw.withdrawldetail);
-    })
+    });
     // //
     it('withdraw() when requested returns an object using the most possible priorty denomination units', function() {
 
 
       // withdrawing 68 from a balance of 468 returns the epxected amount
       $scope.currentbalance = 84468;
-      $scope.float = [ {"denomination": 1, "amount": 8, "type": "coin"}, {"denomination": 20, "amount": 3, "type": "coin"}, { "denomination": 100, "amount": 4, "type": "coin"}, { "denomination": 1000, "amount": 4, "type": "note"},{ "denomination": 2000, "amount": 40, "type": "note"}];
+      $scope.float = [ {'denomination': 1, 'amount': 8, 'type': 'coin'}, {'denomination': 20, 'amount': 3, 'type': 'coin'}, { 'denomination': 100, 'amount': 4, 'type': 'coin'}, { 'denomination': 1000, 'amount': 4, 'type': 'note'}, { 'denomination': 2000, 'amount': 40, 'type': 'note'}];
       // change the withdrawl algorothm
       $scope.changeWithdrawlPriority('d');
       var nw = $scope.withdraw(10268);
 
-      expect(nw.withdrawldetail[3].count).toEqual( Math.floor( 10268/$scope.prioritydenomination));
+      expect(nw.withdrawldetail[3].count).toEqual( Math.floor( 10268 / $scope.prioritydenomination));
 
-    })
+    });
 
     it('deposit() loads the float.json file again resetting the balance of available funds', inject(function ($http, $httpBackend) {
 
@@ -107,18 +112,18 @@ describe('CaashPoint', function () {
 
       $scope.depostFunds = function() {
         $http.get('/data/float.json')
-        .success( function( data, status, headers, config ) {
+        .success( function( data ) {
           $scope.float = data.float;
 
         })
-        .error( function( data, status, headers, config ) {
+        .error( function() {
           $scope.valid = false;
-        })
+        });
       };
 
       $httpBackend.when('GET', '/data/float.json')
-      .respond(200, { float: [ {"denomination": 1, "amount": 100},
-                    {"denomination": 2, "amount": 100} ] });
+      .respond(200, { float: [ {'denomination': 1, 'amount': 100},
+                    {'denomination': 2, 'amount': 100} ] });
 
       $httpBackend.flush();
 
