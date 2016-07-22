cfe075f6378260330b5d520df41d8437325df1ca
pete hotchkiss <pete@pheekaboo.io>
2016-07-22 20:49:29 +0100

    nuked a rouge console.log still floating around

--- app/scripts/directives/ng-alias.js
+++ app/scripts/directives/ng-alias.js
@@ -1,11 +1,12 @@
+'use strict';
+
 function ngAlias($compile) {
     return {
-        restrict: "A",
+        restrict: 'A',
         link: function(scope, element, attrs) {
-            console.log('arrts', attrs);
-            var splits = attrs['ngAlias'].trim().split(/\s+as\s+/);
-            scope.$watch(splits[0], function(val) {
-                scope.$eval(splits[1]+'=('+splits[0]+')');
+            var splits = attrs.ngAlias.trim().split(/\s+as\s+/);
+            scope.$watch(splits[0], function() {
+                scope.$eval(splits[1] + '=(' + splits[0] + ')');
             });
         }
     };
